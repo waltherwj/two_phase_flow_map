b8fe106b338ee872268547a60dd414fdf79e65c9
walther.johnson <walther.johnson@ucalgary.ca>
2021-05-21 17:28:53 -0600

    create config file

---
+++ source/config.py
@@ -0,0 +1,11 @@
+"""
+Configuration file. Contains classes that are used for configuration
+"""
+
+
+class Config:
+    """
+    class for a namespace of the configs
+    """
+
+    NUMBER_DATAPOINTS = 200
