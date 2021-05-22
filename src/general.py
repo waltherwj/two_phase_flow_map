50f52999008993f35b57a876de257d16b358dbc5
walther.johnson <walther.johnson@ucalgary.ca>
2021-05-21 18:59:11 -0600

    add continuous plot graph

--- src/general.py
+++ src/general.py
@@ -2,18 +2,35 @@
 This is used to generate the single fluid velocity maps
 """
 import numpy as np
+import matplotlib.pyplot as plt
 from config import Config
+
+import visualization as vis
 
 
 def generate_velocity_maps(
     datapoints=Config.NUMBER_DATAPOINTS,
     min_uls=Config.MIN_ULS,
     max_uls=Config.MAX_ULS,
-    min_uGs=Config.MIN_UGS,
+    min_ugs=Config.MIN_UGS,
     max_ugs=Config.MAX_UGS,
 ):
+    """create maps of corresponding ugs and uls
+    where ugs is the x axis and uls is the y axis
+    """
 
-    pass
+    # create the arrays
+    ugs_array = np.geomspace(min_ugs, max_ugs, num=datapoints)
+    uls_array = np.geomspace(min_uls, max_uls, num=datapoints)
+
+    # tile them up in the correct dimension
+    ugs_map = np.tile(ugs_array, (datapoints, 1))
+    uls_map = np.tile(uls_array, (datapoints, 1)).T
+
+    return ugs_map, uls_map
 
 
-generate_velocity_maps(1)
+ugs_map, uls_map = generate_velocity_maps()
+vis.plot_continuous_symlog(ugs_map, x_ticks=ugs_map[0, :], y_ticks=uls_map[:, 0])
+
+plt.show(block=True)
--- src/visualization.py
+++ src/visualization.py
@@ -2,3 +2,33 @@
 This module handles the functions that are used to visualize the
 maps and other important features
 """
+import matplotlib.pyplot as plt
+import matplotlib.colors
+
+
+def plot_continuous_symlog(array_map, x_ticks=None, y_ticks=None, thresh=0.1):
+    """
+    plot a map which contains the representation of a
+    continuous variable on a symmetrical log scale
+    (most scenarios for this type of mapping)
+    """
+    # initialize the figure
+    fig, axs = plt.subplots(figsize=(7, 7))
+
+    # handle either using axes or not
+    if (x_ticks is None) or (y_ticks is None):
+        axs.pcolormesh(array_map, norm=matplotlib.colors.SymLogNorm(linthresh=thresh))
+    else:
+        axs.pcolormesh(
+            x_ticks,
+            y_ticks,
+            array_map,
+            norm=matplotlib.colors.SymLogNorm(linthresh=thresh),
+            shading="nearest",
+        )
+        axs.set_xticks(x_ticks)
+        axs.set_yticks(y_ticks)
+        axs.set_xscale("log")
+        axs.set_yscale("log")
+
+    return fig, axs
