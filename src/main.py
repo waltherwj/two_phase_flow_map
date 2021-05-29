1958983f2d2642e826d0408f318f3cb17d9a6418
walther.johnson <walther.johnson@ucalgary.ca>
2021-05-29 00:50:54 -0600

    finish readme

--- src/main.py
+++ src/main.py
@@ -2,32 +2,38 @@
 """
 
 if __name__ == "__main__":
-    import numpy as np
-    from generate_data import generate_velocity_maps
-    from fluids import Liquid, Gas, Pipe
-    from parse_maps import get_categories_maps
+    """
+    example of usage
+    """
+    import matplotlib.pyplot as plt
+    import generate_data
+    import fluids
+    import parse_maps
+    import visualization
 
-    total_mass_flow = 0.01
+    total_mass_flow = 2
     quality = 0.1
     liq_massflow = total_mass_flow * (1 - quality)
     gas_massflow = total_mass_flow * quality
 
-    ugs_temp, uls_temp = generate_velocity_maps()
-    liq_temp = Liquid(
+    ugs_temp, uls_temp = generate_data.generate_velocity_maps()
+    liq_temp = fluids.Liquid(
         density=998,
         bubble_surface_tension=0.073,
         mass_flowrate=liq_massflow,
         dynamic_viscosity=8.9e-4,
     )
-    gas_temp = Gas(density=1.225, mass_flowrate=gas_massflow, dynamic_viscosity=18.3e-6)
+    gas_temp = fluids.Gas(
+        density=1.225, mass_flowrate=gas_massflow, dynamic_viscosity=18.3e-6
+    )
 
-    for inclination in [-90, -80, -30, -1, 0, 1, 30, 80, 90]:
-        pipe_temp = Pipe(diameter=5.1e-2, inclination=inclination, roughness=0.00001)
-        categories = get_categories_maps(
+    for inclination in [1]:  # [-90, -80, -30, -1, 0, 1, 30, 80, 90]:
+        pipe_temp = fluids.Pipe(diameter=0.3, inclination=inclination, roughness=0.001)
+        categories = parse_maps.get_categories_maps(
             ugs_temp, uls_temp, liq_temp, gas_temp, pipe_temp
         )
 
-        fig_temp, ax = plot_map(
+        fig_temp, ax = visualization.plot_map(
             categories,
             liq_temp,
             gas_temp,
--- src/visualization.py
+++ src/visualization.py
@@ -8,6 +8,7 @@ from matplotlib.patches import Patch
 import general
 from scipy.ndimage import gaussian_filter
 import generate_data
+import numpy as np
 
 
 def plot_map(category_map, liquid, gas, pipe, x_ticks=None, y_ticks=None):
@@ -23,7 +24,8 @@ def plot_map(category_map, liquid, gas, pipe, x_ticks=None, y_ticks=None):
     # scale up to 1
     alphas = alphas / alphas.max()
     # scale min alpha
-    alphas[alphas < 0.2] = 0.2
+    min_alpha = 0.2
+    alphas[alphas < min_alpha] = min_alpha
 
     # get the specific location of the supplied case
     u_gs = general.single_fluid_velocity(gas, pipe)
@@ -33,9 +35,11 @@ def plot_map(category_map, liquid, gas, pipe, x_ticks=None, y_ticks=None):
     legend_elements = []
     for category, value in Config.CATEGORIES.items():
         if np.any(category_map == value):
-            legend_elements.append(Patch(facecolor=Config.CMAP(value), label=category))
+            color = list(Config.CMAP(value))
+            color[-1] = min(min_alpha * 3, 1)
+            legend_elements.append(Patch(facecolor=color, label=category))
 
-    c = axs.pcolormesh(
+    axs.pcolormesh(
         x_ticks,
         y_ticks,
         category_map,
@@ -55,41 +59,3 @@ def plot_map(category_map, liquid, gas, pipe, x_ticks=None, y_ticks=None):
     plt.tight_layout()
 
     return fig, axs
-
-
-if __name__ == "__main__":
-    import numpy as np
-    from generate_data import generate_velocity_maps
-    from fluids import Liquid, Gas, Pipe
-    from parse_maps import get_categories_maps
-
-    total_mass_flow = 0.01
-    quality = 0.1
-    liq_massflow = total_mass_flow * (1 - quality)
-    gas_massflow = total_mass_flow * quality
-
-    ugs_temp, uls_temp = generate_velocity_maps()
-    liq_temp = Liquid(
-        density=998,
-        bubble_surface_tension=0.073,
-        mass_flowrate=liq_massflow,
-        dynamic_viscosity=8.9e-4,
-    )
-    gas_temp = Gas(density=1.225, mass_flowrate=gas_massflow, dynamic_viscosity=18.3e-6)
-
-    for inclination in [-90, -80, -30, -1, 0, 1, 30, 80, 90]:
-        pipe_temp = Pipe(diameter=5.1e-2, inclination=inclination, roughness=0.00001)
-        categories = get_categories_maps(
-            ugs_temp, uls_temp, liq_temp, gas_temp, pipe_temp
-        )
-
-        fig_temp, ax = plot_map(
-            categories,
-            liq_temp,
-            gas_temp,
-            pipe_temp,
-            x_ticks=ugs_temp[0, :],
-            y_ticks=uls_temp[:, 0],
-        )
-
-    plt.show(block=True)
